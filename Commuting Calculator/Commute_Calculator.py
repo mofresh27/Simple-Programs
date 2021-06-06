74d0cc25e3f580475c2e754112c3dc40a4e8e6ec
mofresh27 <81204352+mofresh27@users.noreply.github.com>
2021-06-05 22:11:57 -0700

    integer check

--- Commuting Calculator/Commute_Calculator.py
+++ Commuting Calculator/Commute_Calculator.py
@@ -1,24 +1,46 @@
-lifx = int(input("How many years do you plan on commuting?"))
+def commute_calulator():
+    lifx = int(input("How many years do you plan on commuting?\n"))
 
-days = 261 * 24 
-commute = 2/24 
+    try:
+     
+            
+        days = 261 * 24 
+        commute = 2/24 
 
-# print ( days)
-# print ( commute)
+        # print ( days)
+        # print ( commute)
+
+        life = (lifx * days) * commute
+
+        time = life/261
+        daysx = time * 8 
+        years = daysx/365
+        hours = time * 24
+
+
+
+        print ("You will spend approximatley", time, "days commuting to work.")
+        print ("That is equal to", daysx,"workdays.")
+        print ( "Which is", years, " years,")
+        print ("and......", hours ,"hours.")
+        print("                                          ")
+
+        print("That is too much time wasted, go find your beach!")
+
+    except ValueError:
+        print("Invalid Entry, please use a whole number")
+        print("******************************************************")
+        print("[][]][[][][][][][][][][[]][][][][[]][][][][][]][][][][]")
+        print("                                          ")
+        print("                                          ")
+        print("                                          ")
+
+
+        commute_calulator()
+    
 
 
 
 
-life = (lifx * days) * commute
 
-time = life/261
-daysx = time * 8 
-years = daysx/365
-hours = time * 24
-
-
-
-print ("You will spend approximatley", time, "days commuting to work.")
-print ("That is equal to", daysx,"workdays.")
-print ( "Which is", years, " years,")
-print ("and......", hours ,"hours.")
+commute_calulator()
